f219e976044020f0eab0eb9dba05352bfbef0aa1
Joshua Hull <josh@fireflop.com>
2024-01-08 13:19:07 +0100

    Remove unused import

--- src/ui/Main.tsx
+++ src/ui/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { gameStore } from './index.js';
 import Game from './game/Game.js';
 import Setup from './setup/Setup.js';
