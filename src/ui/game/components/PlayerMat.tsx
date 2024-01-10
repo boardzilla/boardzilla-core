60db374614898aa2367270620587a309e80248f7
Joshua Hull <josh@fireflop.com>
2024-01-10 14:53:14 +0100

    Hook up online-ness

--- src/ui/game/components/PlayerMat.tsx
+++ src/ui/game/components/PlayerMat.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import { Player } from '../../../index.js';
-// import { gameStore } from '../../index.js';
+import { gameStore } from '../../index.js';
 
 type PlayerMatProps = {
   player: Player
 }
 
 export function PlayerMat({player}: PlayerMatProps) {
-  // const [userOnline] = gameStore(s => [s.userOnline]);
-  const online = true; //userOnline.has(player.id)
+  const [userOnline] = gameStore(s => [s.userOnline]);
+  const online = userOnline.has(player.id)
   return (
     <div className="player-mat" style={{backgroundColor: player.color}}>
       <div className={`avatar ${online ? "online" : "offline"}`}><img src={player.avatar} /></div>
