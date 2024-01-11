8b33a465fbdb4ec696589950fa47604ff22aa32f
Andrew Hull <aghull@gmail.com>
2024-01-11 10:23:03 -0500

    badge tweaks

--- src/ui/game/components/ProfileBadge.tsx
+++ src/ui/game/components/ProfileBadge.tsx
@@ -10,15 +10,11 @@ type ProfileBadgeProps = {
 
 export function ProfileBadge({player}: ProfileBadgeProps) {
   const [userOnline] = gameStore(s => [s.userOnline, s.boardJSON]);
-  const online = true ; // userOnline.has(player.id)
+  const online = userOnline.has(player.id)
   return (
     <div className={classNames("profile-badge", {online, current: player.isCurrent()})} style={{backgroundColor: player.color}}>
       <div className="avatar"><img src={player.avatar} /></div>
-      <div className="player-name">
-        <div className="player-name-container">
-          <div>{player.name}</div>
-        </div>
-      </div>
+      <div className="player-name">{player.name}</div>
     </div>
   );
 }
